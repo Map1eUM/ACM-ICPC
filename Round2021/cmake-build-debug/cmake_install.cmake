20f2a884cfaf4e9c94193e16f59dbd1c1fe57985
Map1eUM <chenr5@myumanitoba.ca>
2021-11-14 21:17:43 -0600

    merged the Google 2021 kick start RoundG

--- Round2021/cmake-build-debug/cmake_install.cmake
+++ Round2021/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/chenr5-INS/CLionProjects/ACM-ICPC
+# Install script for directory: C:/Users/chenr5-INS/CLionProjects/Round2021
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/untitled1")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Round2021")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/chenr5-INS/CLionProjects/ACM-ICPC/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/chenr5-INS/CLionProjects/Round2021/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
